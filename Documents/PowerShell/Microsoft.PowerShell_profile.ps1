043738e630f538951f45f0bbcc6efa5592652042
Mike Wallio <miwalli@microsoft.com>
2021-04-27 16:44:35 -0400

    Update legacy powershell profile location

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,12 @@
-Import-Module posh-git
-Import-Module oh-my-posh
-Set-Prompt
+foreach ($mod in @('posh-git', 'oh-my-posh', 'Terminal-Icons')) {
+	if (Get-Module -ListAvailable -Name $mod) {
+		Import-Module $mod
+
+		if (($mod -eq 'oh-my-posh') -and (Test-Path "~/.cache/wal/posh-wal.json")) {
+			Set-PoshPrompt -Theme "~/.cache/wal/posh-wal.json"
+		}
+	}
+}
 
 function Get-CommandSource {
 	$(Get-Command $args).Source
@@ -19,21 +25,15 @@ else {
 }
 
 $gitroot = $env:GithubLocation
-if (Test-Path $gitroot\jekyll-pwsh\jekyll.psm1) {
-	Import-Module $gitroot\jekyll-pwsh\jekyll.psm1
-}
-
-if (Test-Path $gitroot\addlicense\AddLicense.psm1) {
-	Import-Module $gitroot\addlicense\AddLicense.psm1
-}
-
-if (Test-Path $gitroot\winwal\winwal.psm1) {
-	Import-Module $gitroot\winwal\winwal.psm1
+foreach ($mod in @("$gitroot\jekyll-pwsh\jekyll.psm1", "$gitroot\addlicense\AddLicense.psm1", "$gitroot\winwal\winwal.psm1")) {
+	if (Test-Path -Path $mod) {
+		Import-Module $mod
+	}
 }
 
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
+if (Test-Path -Path $ChocolateyProfile) {
 	Import-Module "$ChocolateyProfile"
 }
 
@@ -43,3 +43,29 @@ function Update-Config {
 }
 
 Set-Alias -Name config -Value Update-Config
+
+function Invoke-GitHubQuery {
+	param(
+		[string]$Query,
+		[ValidateSet('none', 'Repositories', 'Code', 'Commits', 'Issues', 'Discussions', 'Packages', 'Marketplace', 'Topics', 'Wikis', 'Users')]$Type = 'none',
+		[string]$Language
+	)
+
+	$uri = [System.UriBuilder]::new('https://github.com/search')
+	$params = [System.Web.HttpUtility]::ParseQueryString($uri.Query)
+	if ($Query) {
+		$params.Add('q', $Query)
+	}
+
+	if ($Type -ne 'none') {
+		$params.Add('type', $Type)
+	}
+
+	if ($Language) {
+		$params.Add('l', $Language);
+	}
+
+	$uri.Query = $params.ToString()
+
+	Start-Process $uri.Uri.OriginalString
+}
